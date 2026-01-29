f3868c0264178498ef81cca69c0ef9287e820cf3
milatina <pulse@milatina.io>
2026-01-29 00:06:40 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+const sigil = document.querySelector('.cursor-sigil');
+
+document.addEventListener('mousemove', (e) => {
+    // Moves the sigil to your mouse position
+    sigil.style.transform = `translate3d(${e.clientX - 40}px, ${e.clientY - 40}px, 0) rotate(${window.scrollY / 5}deg)`;
+});
